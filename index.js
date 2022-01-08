fcea774b7aac22db92c6e9690e4cd0adac5f7739
daniep01 <daniep01@users.noreply.github.com>
2022-01-08 12:21:06 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function instance(system) {
 	self.locks = []
 	self.port_labels = []
 
-	for (var i = 1; i <= 8; i ++) {
+	for (var i = 1; i <= 8; i++) {
 		self.port_labels.push({ id: 'A' + i, label: 'Analogue ' + i })
 		self.locks.push({ id: 'A' + i, value: 0 })
 	}
 
-	for (var i = 1; i <= 16; i ++) {
+	for (var i = 1; i <= 16; i++) {
 		self.port_labels.push({ id: 'D' + i, label: 'Dante ' + i })
 		self.locks.push({ id: 'D' + i, value: 0 })
 	}
@@ -34,29 +34,27 @@ function instance(system) {
 	return self
 }
 
-instance.prototype.updateConfig = function(config) {
+instance.prototype.updateConfig = function (config) {
 	var self = this
 
 	self.config = config
 	self.init_tcp()
+}
 
-};
+instance.prototype.init = function () {
+	var self = this
 
-instance.prototype.init = function() {
-	var self = this;
-
-	debug = self.debug;
-	log = self.log;
+	debug = self.debug
+	log = self.log
 
 	self.init_tcp()
 
 	self.create_variables()
 	self.init_presets()
 	self.update_variables()
+}
 
-};
-
-instance.prototype.init_tcp = function() {
+instance.prototype.init_tcp = function () {
 	var self = this
 	var receivebuffer = ''
 
@@ -68,47 +66,49 @@ instance.prototype.init_tcp = function() {
 	self.has_data = false
 
 	if (self.config.host) {
-		self.socket = new tcp(self.config.host, self.config.port);
+		self.socket = new tcp(self.config.host, self.config.port)
 
 		self.socket.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
+			self.status(status, message)
+		})
 
 		self.socket.on('error', function (err) {
-			debug('Network error', err);
-			self.log('error', 'Network error: ' + err.message);
-		});
+			debug('Network error', err)
+			self.log('error', 'Network error: ' + err.message)
+		})
 
 		self.socket.on('connect', function () {
-			debug('Connected');
-		});
+			debug('Connected')
+		})
 
 		// separate buffered stream into lines with responses
 		self.socket.on('data', function (chunk) {
-			self.log('debug','data received')
-			var i = 0, line = '', offset = 0;
-			receivebuffer += chunk;
+			self.log('debug', 'data received')
+			var i = 0,
+				line = '',
+				offset = 0
+			receivebuffer += chunk
 
-			while ( (i = receivebuffer.indexOf('\n', offset)) !== -1) {
-				line = receivebuffer.substr(offset, i - offset);
-				offset = i + 1;
-				self.socket.emit('receiveline', line.toString());
+			while ((i = receivebuffer.indexOf('\n', offset)) !== -1) {
+				line = receivebuffer.substr(offset, i - offset)
+				offset = i + 1
+				self.socket.emit('receiveline', line.toString())
 			}
 
 			receivebuffer = receivebuffer.substr(offset)
-		});
+		})
 
 		self.socket.on('receiveline', function (line) {
-				debug('Response from device: ', line)
-				if (line != 'OK') {
-					self.log('debug', line.toString())
-				}
-		});
+			debug('Response from device: ', line)
+			if (line != 'OK') {
+				self.log('debug', line.toString())
+			}
+		})
 	}
-};
+}
 
 instance.prototype.config_fields = function () {
-	var self = this;
+	var self = this
 
 	return [
 		{
@@ -134,57 +134,53 @@ instance.prototype.config_fields = function () {
 			regex: self.REGEX_PORT,
 		},
 	]
-};
+}
 
-instance.prototype.destroy = function() {
+instance.prototype.destroy = function () {
 	var self = this
 
 	if (self.socket !== undefined) {
 		self.socket.destroy()
 	}
-};
+}
 
 instance.prototype.create_variables = function (system) {
 	var self = this
 	var variables = []
-	
-	for (var i = 0; i < self.locks.length; i ++)
-	{
+
+	for (var i = 0; i < self.locks.length; i++) {
 		variables.push({
 			label: 'Lock state ' + self.locks[i].id,
-			name: 'lock_state_'  + self.locks[i].id,
-		});
+			name: 'lock_state_' + self.locks[i].id,
+		})
 	}
 
-	self.setVariableDefinitions(variables);
-};
+	self.setVariableDefinitions(variables)
+}
 
 instance.prototype.update_variables = function (system) {
 	var self = this
-	
-	for (var i = 0; i < self.locks.length; i ++)
-	{
+
+	for (var i = 0; i < self.locks.length; i++) {
 		self.setVariable('lock_state_' + self.locks[i].id, self.locks[i].value)
 	}
-};
+}
 
-instance.prototype.feedback = function(feedback, bank) {
-	var self = this;
-
-};
+instance.prototype.feedback = function (feedback, bank) {
+	var self = this
+}
 
 instance.prototype.init_presets = function () {
-	var self = this;
-	var presets = [];
-	self.setPresetDefinitions(presets);
-};
+	var self = this
+	var presets = []
+	self.setPresetDefinitions(presets)
+}
 
-instance.prototype.actions = function() {
-	var self = this;
+instance.prototype.actions = function () {
+	var self = this
 
 	self.system.emit('instance_actions', self.id, {
-
-		'crosspoint': {
+		crosspoint: {
 			label: 'Connect',
 			options: [
 				{
@@ -201,9 +197,9 @@ instance.prototype.actions = function() {
 					default: 'D1',
 					choices: self.port_labels,
 				},
-			]
+			],
 		},
-		'disconnect': {
+		disconnect: {
 			label: 'Disconnect',
 			options: [
 				{
@@ -220,9 +216,9 @@ instance.prototype.actions = function() {
 					default: 'D1',
 					choices: self.port_labels,
 				},
-			]
+			],
 		},
-		'lock': {
+		lock: {
 			label: 'Set destination lock',
 			options: [
 				{
@@ -240,12 +236,12 @@ instance.prototype.actions = function() {
 					choices: [
 						{ id: '1', label: 'Lock Destination' },
 						{ id: '0', label: 'Unlock Destination' },
-						{ id: 't', label: 'Toggle Lock' }
+						{ id: 't', label: 'Toggle Lock' },
 					],
 				},
-			]
+			],
 		},
-		'input_level': {
+		input_level: {
 			label: 'Adjust input level',
 			options: [
 				{
@@ -261,11 +257,11 @@ instance.prototype.actions = function() {
 					id: 'input_level',
 					default: 0,
 					min: -12,
-					max: 12
-				}
-			]
+					max: 12,
+				},
+			],
 		},
-		'output_level': {
+		output_level: {
 			label: 'Adjust output level',
 			options: [
 				{
@@ -281,14 +277,14 @@ instance.prototype.actions = function() {
 					id: 'output_level',
 					default: 0,
 					min: -12,
-					max: 12
-				}
-			]
+					max: 12,
+				},
+			],
 		},
-		'status': {
+		status: {
 			label: 'Ask device for status',
 		},
-		'reset': {
+		reset: {
 			label: 'Reset',
 			options: [
 				{
@@ -298,31 +294,30 @@ instance.prototype.actions = function() {
 					default: 'all off',
 					choices: [
 						{ id: 'all off', label: 'Reset all routing' },
-						{ id: 'all zero', label: 'Reset all levels to zero' }
+						{ id: 'all zero', label: 'Reset all levels to zero' },
 						// { id: 'all unlock', label: 'Unlock all destinations' }
-					]
+					],
 				},
-			]
+			],
 		},
-	});
+	})
 }
 
-instance.prototype.action = function(action) {
-
+instance.prototype.action = function (action) {
 	var self = this
 	var cmd
 
 	if (action.action === 'crosspoint') {
-		objIndex = self.locks.findIndex((obj => obj.id == action.options.destination))
+		objIndex = self.locks.findIndex((obj) => obj.id == action.options.destination)
 		if (self.locks[objIndex].value == 1) {
 			self.log('warn', 'Unable to route to ' + action.options.destination + ' because it is locked')
 		} else {
 			cmd = action.options.source + '>' + action.options.destination + 'On\r\n'
 		}
 	}
-	
+
 	if (action.action === 'disconnect') {
-		objIndex = self.locks.findIndex((obj => obj.id == action.options.disconnect_destination))
+		objIndex = self.locks.findIndex((obj) => obj.id == action.options.disconnect_destination)
 		if (self.locks[objIndex].value == 1) {
 			self.log('warn', 'Unable to disconnect ' + action.options.disconnect_destination + ' because it is locked')
 		} else {
@@ -332,11 +327,11 @@ instance.prototype.action = function(action) {
 
 	if (action.action === 'lock') {
 		self.debug('set dest ' + action.options.lock_destination + ' to lock value ' + action.options.lock)
-		objIndex = self.locks.findIndex((obj => obj.id == action.options.lock_destination))
-		
+		objIndex = self.locks.findIndex((obj) => obj.id == action.options.lock_destination)
+
 		if (action.options.lock === 't') {
 			// toggle
-			self.locks[objIndex].value = (self.locks[objIndex].value == 0 ? 1 : 0)
+			self.locks[objIndex].value = self.locks[objIndex].value == 0 ? 1 : 0
 		} else {
 			self.locks[objIndex].value = action.options.lock
 		}
@@ -355,7 +350,7 @@ instance.prototype.action = function(action) {
 	if (action.action === 'reset') {
 		cmd = action.options.reset + '\r\n'
 	}
-	
+
 	if (action.action === 'status') {
 		cmd = 'status\r\n'
 	}
@@ -365,11 +360,11 @@ instance.prototype.action = function(action) {
 			self.debug('sending: ' + cmd)
 			self.socket.send(cmd)
 		} else {
-			self.log('warn','Socket not connected')
+			self.log('warn', 'Socket not connected')
 			self.debug('Socket not connected, tried to send: ' + cmd)
 		}
 	}
-};
+}
 
 instance_skel.extendedBy(instance)
 exports = module.exports = instance
