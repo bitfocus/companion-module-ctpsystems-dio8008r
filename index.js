d7d03a6ab8dd2cb76b51e6ab86ff3be2eb63770d
Peter Daniel <peterdaniel@peters-macbook-air.local>
2023-09-11 21:00:44 +0100

    v2

--- index.js
+++ index.js
@@ -1,478 +1,139 @@
 // CTP Systems DIO8008R or DIO8008RX
 
-var tcp = require('../../tcp')
-var instance_skel = require('../../instance_skel')
-var debug
-var log
+import { InstanceBase, InstanceStatus, runEntrypoint, TCPHelper, UDPHelper } from '@companion-module/base'
+import { ConfigFields } from './config.js'
+import { updateActions } from './actions.js'
+import { updatePresets } from './presets.js'
+import { updateVariables } from './variables.js'
 
-function instance(system) {
-	var self = this
+class CTP_DIO8008 extends InstanceBase {
+	constructor(internal) {
+		super(internal)
 
-	// Request id counter
-	self.request_id = 0
-	self.stash = []
-	self.command = null
+		this.updateActions = updateActions.bind(this)
+		this.updatePresets = updatePresets.bind(this)
+		this.updateVariables = updateVariables.bind(this)
 
-	self.locks = []
-	self.source_port_labels = []
-	self.dest_port_labels = []
-
-	for (var i = 1; i <= 8; i++) {
-		self.source_port_labels.push({ id: 'A' + i, label: 'Analogue ' + i })
 	}
 
-	for (var i = 1; i <= 16; i++) {
-		self.source_port_labels.push({ id: 'D' + i, label: 'Dante ' + i })
+	async init(config) {
+		this.config = config
+
+		this.source_port_labels = []
+		this.dest_port_labels = []
+
+		for (var i = 1; i <= 8; i++) {
+			this.source_port_labels.push({ id: 'A' + i, label: 'Analogue ' + i })
+		}
+
+		for (var i = 1; i <= 16; i++) {
+			this.source_port_labels.push({ id: 'D' + i, label: 'Dante ' + i })
+		}
+
+		for (var i = 1; i <= 8; i++) {
+			this.dest_port_labels.push({ id: 'A' + i, label: 'Analogue ' + i })
+		}
+
+		for (var i = 1; i <= 16; i++) {
+			this.dest_port_labels.push({ id: 'D' + i, label: 'Dante ' + i })
+		}
+
+		this.updateActions()
+		this.updateVariables()
+		this.updatePresets()
+
+		await this.configUpdated(config)
 	}
 
-	for (var i = 1; i <= 8; i++) {
-		self.dest_port_labels.push({ id: 'A' + i, label: 'Analogue ' + i })
-		self.locks.push({ id: 'A' + i, value: 0 })
+	async configUpdated(config) {
+		if (this.udp) {
+			this.udp.destroy()
+			delete this.udp
+		}
+
+		if (this.socket) {
+			this.socket.destroy()
+			delete this.socket
+		}
+
+		this.config = config
+
+		this.init_tcp()
+		this.updateVariables()
+
 	}
 
-	for (var i = 1; i <= 16; i++) {
-		self.dest_port_labels.push({ id: 'D' + i, label: 'Dante ' + i })
-		self.locks.push({ id: 'D' + i, value: 0 })
+	async destroy() {
+		if (this.socket) {
+			this.socket.destroy()
+		} else if (this.udp) {
+			this.udp.destroy()
+		} else {
+			this.updateStatus(InstanceStatus.Disconnected)
+		}
 	}
 
-	// super-constructor
-	instance_skel.apply(this, arguments)
-
-	self.actions()
-
-	return self
-}
-
-instance.prototype.updateConfig = function (config) {
-	var self = this
-
-	self.config = config
-	self.init_tcp()
-}
-
-instance.prototype.init = function () {
-	var self = this
-
-	debug = self.debug
-	log = self.log
-
-	self.init_tcp()
-
-	self.create_variables()
-	self.init_presets()
-	self.update_variables()
-}
-
-instance.prototype.init_tcp = function () {
-	var self = this
-	var receivebuffer = ''
-
-	if (self.socket !== undefined) {
-		self.socket.destroy()
-		delete self.socket
+	getConfigFields() {
+		return ConfigFields
 	}
 
-	self.has_data = false
+	init_tcp() {
+		console.log('initTCP ' + this.config.host + ':' + this.config.port)
+		
+		if (this.socket) {
+			this.socket.destroy()
+			delete this.socket
+		}
 
-	if (self.config.host) {
-		self.socket = new tcp(self.config.host, self.config.port)
+		this.updateStatus(InstanceStatus.Connecting)
 
-		self.socket.on('status_change', function (status, message) {
-			self.status(status, message)
-		})
+		if (this.config.host) {
+			this.socket = new TCPHelper(this.config.host, this.config.port)
 
-		self.socket.on('error', function (err) {
-			debug('Network error', err)
-			self.log('error', 'Network error: ' + err.message)
-		})
+			this.socket.on('status_change', (status, message) => {
+				this.updateStatus(status, message)
+			})
 
-		self.socket.on('connect', function () {
-			debug('Connected')
-		})
+			this.socket.on('error', (err) => {
+				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+				this.log('error', 'Network error: ' + err.message)
+			})
 
-		// separate buffered stream into lines with responses
-		self.socket.on('data', function (chunk) {
-			self.log('debug', 'data received')
-			var i = 0,
-				line = '',
-				offset = 0
-			receivebuffer += chunk
+			this.socket.on('data', (data) => {
+				console.log('Data received')
+				console.log(data)
 
-			while ((i = receivebuffer.indexOf('\n', offset)) !== -1) {
-				line = receivebuffer.substr(offset, i - offset)
-				offset = i + 1
-				self.socket.emit('receiveline', line.toString())
+				var i = 0,
+					line = '',
+					offset = 0
+					receivebuffer += data
+
+				while ((i = receivebuffer.indexOf('\n', offset)) !== -1) {
+					line = receivebuffer.substr(offset, i - offset)
+					offset = i + 1
+					console.log('Response from device: ', line)
+					if (line != 'OK') {
+						this.log('debug', line.toString())
+					}
+				}
+				receivebuffer = receivebuffer.substr(offset)
+			})
+		} else {
+			this.updateStatus(InstanceStatus.BadConfig)
+		}
+	}
+
+	sendCommand(cmd) {
+		console.log('Trying to send: ' + cmd)
+		if (cmd !== undefined) {
+			if (this.socket !== undefined) {
+				this.socket.send(cmd)
+			} else {
+				this.log('warn', 'Socket not connected, tried to send: ' + cmd)
 			}
-
-			receivebuffer = receivebuffer.substr(offset)
-		})
-
-		self.socket.on('receiveline', function (line) {
-			debug('Response from device: ', line)
-			if (line != 'OK') {
-				self.log('debug', line.toString())
-			}
-		})
-	}
-}
-
-instance.prototype.config_fields = function () {
-	var self = this
-
-	return [
-		{
-			type: 'text',
-			id: 'info',
-			width: 12,
-			label: 'Information',
-			value: 'This module will allow you to control the CTP Systems DIO8008R or DIO8008RX.',
-		},
-		{
-			type: 'textinput',
-			id: 'host',
-			label: 'Device IP',
-			width: 6,
-			regex: self.REGEX_IP,
-		},
-		{
-			type: 'textinput',
-			id: 'port',
-			label: 'Device Port',
-			width: 6,
-			default: '10001',
-			regex: self.REGEX_PORT,
-		},
-	]
-}
-
-instance.prototype.destroy = function () {
-	var self = this
-
-	if (self.socket !== undefined) {
-		self.socket.destroy()
-	}
-}
-
-instance.prototype.create_variables = function (system) {
-	var self = this
-	var variables = []
-
-	for (var i = 0; i < self.locks.length; i++) {
-		variables.push({
-			label: 'Lock state ' + self.locks[i].id,
-			name: 'lock_state_' + self.locks[i].id,
-		})
-	}
-
-	for (var i = 1; i <= 8; i++) {
-		variables.push({
-			label: 'Label source A' + i,
-			name: 'label_source_A' + i,
-		})
-	}
-
-	for (var i = 1; i <= 16; i++) {
-		variables.push({
-			label: 'Label source D' + i,
-			name: 'label_source_D' + i,
-		})
-	}
-
-	for (var i = 1; i <= 8; i++) {
-		variables.push({
-			label: 'Label destination A' + i,
-			name: 'label_dest_A' + i,
-		})
-	}
-
-	for (var i = 1; i <= 16; i++) {
-		variables.push({
-			label: 'Label destination D' + i,
-			name: 'label_dest_D' + i,
-		})
-	}
-
-	self.setVariableDefinitions(variables)
-}
-
-instance.prototype.update_variables = function (system) {
-	var self = this
-
-	for (var i = 0; i < self.locks.length; i++) {
-		self.setVariable('lock_state_' + self.locks[i].id, self.locks[i].value)
-	}
-
-	for (var i = 0; i < self.source_port_labels.length; i++) {
-		self.setVariable('label_source_' + self.source_port_labels[i].id, self.source_port_labels[i].label)
-	}
-	for (var i = 0; i < self.dest_port_labels.length; i++) {
-		self.setVariable('label_dest_' + self.dest_port_labels[i].id, self.dest_port_labels[i].label)
-	}
-}
-
-instance.prototype.feedback = function (feedback, bank) {
-	var self = this
-}
-
-instance.prototype.init_presets = function () {
-	var self = this
-	var presets = []
-	self.setPresetDefinitions(presets)
-}
-
-instance.prototype.actions = function () {
-	var self = this
-
-	self.setActions({
-		crosspoint: {
-			label: 'Connect',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Source',
-					id: 'source',
-					default: 'A1',
-					choices: self.source_port_labels,
-				},
-				{
-					type: 'dropdown',
-					label: 'Destination',
-					id: 'destination',
-					default: 'D1',
-					choices: self.dest_port_labels,
-				},
-			],
-		},
-		disconnect: {
-			label: 'Disconnect',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Source',
-					id: 'disconnect_source',
-					default: 'A1',
-					choices: self.source_port_labels,
-				},
-				{
-					type: 'dropdown',
-					label: 'Destination',
-					id: 'disconnect_destination',
-					default: 'D1',
-					choices: self.dest_port_labels,
-				},
-			],
-		},
-		lock: {
-			label: 'Set destination lock',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Destination',
-					id: 'lock_destination',
-					default: 'A1',
-					choices: self.dest_port_labels,
-				},
-				{
-					type: 'dropdown',
-					label: 'Set',
-					id: 'lock',
-					default: '1',
-					choices: [
-						{ id: '1', label: 'Lock Destination' },
-						{ id: '0', label: 'Unlock Destination' },
-						{ id: 't', label: 'Toggle Lock' },
-					],
-				},
-			],
-		},
-		input_level: {
-			label: 'Adjust input level',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Input',
-					id: 'input_level_port',
-					default: 'A1',
-					choices: self.source_port_labels,
-				},
-				{
-					type: 'number',
-					label: 'Level',
-					id: 'input_level',
-					default: 0,
-					min: -12,
-					max: 12,
-				},
-			],
-		},
-		output_level: {
-			label: 'Adjust output level',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Output',
-					id: 'output_level_port',
-					default: 'A1',
-					choices: self.dest_port_labels,
-				},
-				{
-					type: 'number',
-					label: 'Level',
-					id: 'output_level',
-					default: 0,
-					min: -12,
-					max: 12,
-				},
-			],
-		},
-		status: {
-			label: 'Ask device for status',
-		},
-		user_source_label: {
-			label: 'Change source label',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Port',
-					id: 'source_label_port',
-					default: 'A1',
-					choices: self.source_port_labels,
-				},
-				{
-					type: 'textinput',
-					id: 'source_label_text',
-					label: 'New label',
-					regex: '/^.{1,30}$/',
-				},
-			],
-		},
-		user_dest_label: {
-			label: 'Change destination label',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Port',
-					id: 'dest_label_port',
-					default: 'A1',
-					choices: self.dest_port_labels,
-				},
-				{
-					type: 'textinput',
-					id: 'dest_label_text',
-					label: 'New label',
-					regex: '/^.{1,30}$/',
-				},
-			],
-		},
-		reset: {
-			label: 'Reset',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Action',
-					id: 'reset',
-					default: 'all off',
-					choices: [
-						{ id: 'all off', label: 'Reset all routing' },
-						{ id: 'all zero', label: 'Reset all levels to zero' },
-						// { id: 'all unlock', label: 'Unlock all destinations' }
-					],
-				},
-			],
-		},
-	})
-}
-
-instance.prototype.action = function (action) {
-	var self = this
-	var cmd
-
-	if (action.action === 'crosspoint') {
-		objIndex = self.locks.findIndex((obj) => obj.id == action.options.destination)
-		if (self.locks[objIndex].value == 1) {
-			self.log('warn', 'Unable to route to ' + action.options.destination + ' because it is locked')
-		} else {
-			cmd = action.options.source + '>' + action.options.destination + 'On\r\n'
 		}
 	}
 
-	if (action.action === 'disconnect') {
-		objIndex = self.locks.findIndex((obj) => obj.id == action.options.disconnect_destination)
-		if (self.locks[objIndex].value == 1) {
-			self.log('warn', 'Unable to disconnect ' + action.options.disconnect_destination + ' because it is locked')
-		} else {
-			cmd = action.options.disconnect_source + '>' + action.options.disconnect_destination + 'Off\r\n'
-		}
-	}
-
-	if (action.action === 'lock') {
-		self.debug('set dest ' + action.options.lock_destination + ' to lock value ' + action.options.lock)
-		objIndex = self.locks.findIndex((obj) => obj.id == action.options.lock_destination)
-
-		if (action.options.lock === 't') {
-			// toggle
-			self.locks[objIndex].value = self.locks[objIndex].value == 0 ? 1 : 0
-		} else {
-			self.locks[objIndex].value = action.options.lock
-		}
-		self.update_variables()
-		self.debug(self.locks)
-	}
-
-	if (action.action === 'input_level') {
-		cmd = 'I ' + action.options.input_level_port + '=' + action.options.input_level + '\r\n'
-	}
-
-	if (action.action === 'output_level') {
-		cmd = 'O ' + action.options.output_level_port + '=' + action.options.output_level + '\r\n'
-	}
-
-	if (action.action === 'user_source_label') {
-		if (action.options.source_label_text.length > 0) {
-			self.debug(action.options.source_label_port + ' changing to ' + action.options.source_label_text)
-			objIndex = self.source_port_labels.findIndex((obj) => obj.id == action.options.source_label_port)
-			self.debug(objIndex)
-			self.source_port_labels[objIndex].label = action.options.source_label_text
-			self.update_variables()
-			self.actions()
-			self.debug(self.source_port_labels)
-		} else {
-			self.log('warn', 'New label for source ' + action.options.source_label_port + ' cannot be empty')
-		}
-	}
-
-	if (action.action === 'user_dest_label') {
-		if (action.options.dest_label_text.length > 0) {
-			self.debug(action.options.dest_label_port + ' changing to ' + action.options.dest_label_text)
-			objIndex = self.dest_port_labels.findIndex((obj) => obj.id == action.options.dest_label_port)
-			self.debug(objIndex)
-			self.dest_port_labels[objIndex].label = action.options.dest_label_text
-			self.update_variables()
-			self.actions()
-			self.debug(self.dest_port_labels)
-		} else {
-			self.log('warn', 'New label for destination ' + action.options.dest_label_port + ' cannot be empty')
-		}
-	}
-
-	if (action.action === 'reset') {
-		cmd = action.options.reset + '\r\n'
-	}
-
-	if (action.action === 'status') {
-		cmd = 'status\r\n'
-	}
-
-	if (cmd !== undefined) {
-		if (self.socket !== undefined && self.socket.connected) {
-			self.debug('sending: ' + cmd)
-			self.socket.send(cmd)
-		} else {
-			self.log('warn', 'Socket not connected')
-			self.debug('Socket not connected, tried to send: ' + cmd)
-		}
-	}
 }
 
-instance_skel.extendedBy(instance)
-exports = module.exports = instance
+runEntrypoint(CTP_DIO8008, [])
